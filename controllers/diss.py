4899e544cec167ec9c3e653b752280616bd9a1a6
evanjh <71201536+evanjh@users.noreply.github.com>
2022-03-13 02:59:40 +0800

    Update diss.py

--- controllers/diss.py
+++ controllers/diss.py
@@ -12,16 +12,18 @@ async def handler(args: Args, client: Client, msg: Message):
     m = await msg.edit_text('獲取中...')
     try:
         r = await s.get("https://zuan.shabi.workers.dev")
+        await m.delete()
         if (r.status / 200) == 1:
             text = f'**{await r.text()}**'
-            
             if msg.reply_to_message:
-                text += f' [@{msg.reply_to_message.from_user.first_name}](tg://user?id={msg.reply_to_message.from_user.id})'
+                text += f' [😘](tg://user?id={msg.reply_to_message.from_user.id})'
                 await client.send_message(msg.chat.id, text, 'MD', reply_to_message_id=msg.reply_to_message.message_id)
             else:
                 await client.send_message(msg.chat.id, text, 'MD')
+                
     except:
-        return await client.send_message(msg.chat.id, '失敗', 'MD', disable_web_page_preview=True)
+        await client.send_message(msg.chat.id, '失敗', 'MD', disable_web_page_preview=True)
+        return 
       
       
-    await m.delete()
+    
